f3da0e17047614bf981c10b831d837c5fcdb9816
TaoLoading <1377226380@qq.com>
2021-09-09 11:29:06 +0800

    栈

---
+++ 01-数据结构/01-stack/01-出入栈.js
@@ -0,0 +1,9 @@
+/* JavaScript没有栈的概念，但可以使用Array实现栈的功能 */
+
+const stack = []
+// 入栈
+stack.push(1)
+stack.push(2)
+// 出栈
+const item1 = stack.pop()
+const item2 = stack.pop()
---
+++ 01-数据结构/01-stack/02-函数堆栈.js
@@ -0,0 +1,10 @@
+/* 通过调试观察函数入栈和出栈顺序 */
+const func1 = () => {
+  func2()
+}
+const func2 = () => {
+  func3()
+}
+const func3 = () => { }
+
+func1()
