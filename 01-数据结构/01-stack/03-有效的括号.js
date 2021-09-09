f715df7162f8c35c72c7c1d341d2ec06b6a7876f
TaoLoading <1377226380@qq.com>
2021-09-09 14:02:14 +0800

    栈

--- 01-数据结构/01-stack/03-有效的括号.js
+++ 01-数据结构/01-stack/03-有效的括号.js
@@ -1,5 +1,3 @@
-// 此解题方式时间复杂度和空间复杂度都为
-
 var isValid = function (s) {
   if (s.length % 2 == 1) return false
   const stack = []
@@ -24,3 +22,6 @@ var isValid = function (s) {
   }
   return stack.length === 0
 }
+
+// 时间复杂度为O(n)，因为存在一个for循环，n为s.length
+// 空间复杂度为O(n)，因为存在一个stack栈，极端情况时可能将s的全部即n个元素全部放进去
