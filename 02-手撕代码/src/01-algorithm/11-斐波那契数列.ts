5aeb2539101ebff1735e4fb36e934cb0a09304f4
TaoLoading <1377226380@qq.com>
2022-08-08 10:06:10 +0800

    回顾斐波那契数列

--- 02-手撕代码/src/01-algorithm/11-斐波那契数列.ts
+++ 02-手撕代码/src/01-algorithm/11-斐波那契数列.ts
@@ -6,7 +6,7 @@
  * 递归方式
  * @param n 下标
  * @returns 值
- * 时间复杂度是O(2^n)，存在重复计算
+ * 时间复杂度是O(2^n)，存在大量重复计算
  */
 export function fibonacci1(n: number): number {
   // 特殊值处理
@@ -35,15 +35,17 @@ export function fibonacci2(n: number): number {
     return 1
   }
 
-  // 记录 n-1 的结果
+  // 记录n-1的结果
   let n1 = 1
-  // 记录 n-2 的结果
+  // 记录n-2的结果
   let n2 = 0
-  // 记录 n1 + n2 的结果，即下一个值
+  // 记录n1+n2的结果，即下一个值
   let res = 0
 
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i <= n; i++) { // 注意i从2开始
     res = n1 + n2
+
+    // 更新n2和n1的值
     n2 = n1
     n1 = res
   }
