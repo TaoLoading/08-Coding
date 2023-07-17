ac3e7d19baeaf0b625a3b5d6f9773d5885778587
TaoLoading <1377226380@qq.com>
2023-07-17 11:22:10 +0800

    反转二叉树

--- src/01-算法/19-反转二叉树.test.ts
+++ src/01-算法/19-反转二叉树.test.ts
@@ -1,5 +1,70 @@
 import { describe, expect, it } from 'vitest'
-import { invertBinaryTree1 } from './19-反转二叉树'
+import { invertBinaryTree1, invertBinaryTree2 } from './19-反转二叉树'
 
 describe('反转二叉树', () => {
+  it('正常', () => {
+    const binaryTree = {
+      val: '1',
+      left: {
+        val: '2-1',
+        left: {
+          val: '3-1-1',
+          left: null,
+          right: null
+        },
+        right: {
+          val: '3-1-2',
+          left: null,
+          right: null
+        }
+      },
+      right: {
+        val: '2-2',
+        left: {
+          val: '3-2-1',
+          left: null,
+          right: null
+        },
+        right: {
+          val: '3-2-2',
+          left: null,
+          right: null
+        }
+      }
+    }
+    const res = invertBinaryTree2(binaryTree)
+    expect(res).toEqual({
+      val: '1',
+      left: {
+        val: '2-2',
+        left: {
+          val: '3-2-2',
+          left: null,
+          right: null
+        },
+        right: {
+          val: '3-2-1',
+          left: null,
+          right: null
+        }
+      },
+      right: {
+        val: '2-1',
+        left: {
+          val: '3-1-2',
+          left: null,
+          right: null
+        },
+        right: {
+          val: '3-1-1',
+          left: null,
+          right: null
+        }
+      }
+    })
+  })
+  it('空二叉树', () => {
+    const res = invertBinaryTree2(null)
+    expect(res).toBeNull()
+  })
 })
--- src/01-算法/19-反转二叉树.ts
+++ src/01-算法/19-反转二叉树.ts
@@ -10,18 +10,21 @@ interface binaryTreeType {
 }
 
 /**
- * 递归
- * @param root 
- * @returns 
+ * 使用递归
+ * @param root 二叉树的根节点
+ * @returns 反转后的二叉树
  */
 export function invertBinaryTree1(root: binaryTreeType | null): binaryTreeType | null {
   if (root === null) {
     return root
   }
-  const left = invertBinaryTree1(root.left)
-  const right = invertBinaryTree1(root.right)
-  root.left = right
-  root.right = left
+
+  const temp = root.left
+  root.left = root.right
+  root.right = temp
+  invertBinaryTree1(root.left)
+  invertBinaryTree1(root.right)
+
   return root
 }
 
@@ -54,5 +57,38 @@ const binaryTree = {
     }
   }
 }
+
+/**
+ * 使用广度优先遍历的思路。广度优先遍历体现在层级遍历上，先同级，再子级
+ * @param root 二叉树的根节点
+ * @returns 反转后的二叉树
+ */
+export function invertBinaryTree2(root: binaryTreeType | null): binaryTreeType | null {
+  if (root === null) {
+    return root
+  }
+
+  // 1. 新建队列，根节点入队列
+  const queue: binaryTreeType[] = [root]
+  while (queue.length > 0) {
+    // 2. 依次去除节点，并交换左右子节点
+    const current = queue.shift()!
+    const temp = current.left
+    current.left = current.right
+    current.right = temp
+
+    // 3. 对子节点进行处理
+    if (current.left !== null) {
+      queue.push(current.left)
+    }
+    if (current.right !== null) {
+      queue.push(current.right)
+    }
+  }
+
+  return root
+}
+
 console.log('原二叉树', binaryTree)
-console.log('反转后', invertBinaryTree1(binaryTree))
+// console.log('反转后', invertBinaryTree1(binaryTree))
+console.log('反转后', invertBinaryTree2(binaryTree))
