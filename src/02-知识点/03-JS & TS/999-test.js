f1c318c5afa4453ffca96fa95b03c9834daf844f
TaoLoading <1377226380@qq.com>
2023-09-13 16:50:22 +0800

    堆栈

--- src/02-知识点/03-JS & TS/999-test.js
+++
@@ -1,8 +0,0 @@
-const prototypeObj = {
-  property1: 'value1',
-  property2: 'value2',
-  method1: function () {
-  }
-}
-const obj2 = Object.create(prototypeObj)
-console.log('===', obj2.__proto__)
