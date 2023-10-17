84a8afde81865869d87f145d5fb9ca8910e468e9
TaoLoading <1377226380@qq.com>
2023-10-17 09:20:06 +0800

    增加配置文件

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    host: 'localhost',
+    port: 8888
+  }
+})
